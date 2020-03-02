eecbbe518479d48a4e73305c13326e839cd9602a
DGriffin91 <33357138+DGriffin91@users.noreply.github.com>
2020-03-01 19:42:11 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="Dynload",
+    version="0.1",
+    packages=["dynload",],
+    license="MIT",
+    long_description=open("README.txt").read(),
+)
